68e5a9b81449926ffb246432d9d8e5223879702b
Pâmella Namie Nomura <90418544+Baabie@users.noreply.github.com>
2024-03-20 12:08:58 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,33 @@
+/* 
+
+GC$ 50,00
+GC$ 10,00
+GC$ 5,00
+GC$ 1,00 
+
+*/
+
+function calcularNotas (valor) {
+    const notasDisponiveis = [50, 10 , 5, 1];
+    let quantidadeNotas = {};
+
+    let i = 0;
+    while(valor > 0 && i < notasDisponiveis.length) {
+        const nota = notasDisponiveis[i];
+        quantidadeNotas[nota] = Math.floor(valor / nota);
+        valor %= nota;
+        i++;
+    }
+
+    return quantidadeNotas;
+}
+
+let valorDesejado = parseFloat(prompt('Quanto deseja retirar ? '));
+
+const notasNecessarias = calcularNotas(valorDesejado); 
+
+console.log('Valor desejado:', valorDesejado);
+console.log('Número de notas necessárias: ');
+for (let nota in notasNecessarias) {
+    console.log(`Nota de GC$ ${nota}: ${notasNecessarias[nota]}`);
+}
